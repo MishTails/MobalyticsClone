0d73859e024ca13ce866f4edb366a5b444b71bc0
Jarrod Mishima <mishimajam@gmail.com>
2022-12-14 17:40:53 -0800

    augment + carousel priority model

---
+++ app/models/augment.py
@@ -0,0 +1,20 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Augment(db.Model):
+  __tablename__ = 'augments'
+
+  if environment == "production":
+      __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  tier = db.Column(db.String)
+  desc = db.Column(db.String)
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'tier': self.tier,
+      'desc': self.desc
+    }
--- app/models/buildergrid.py
+++ app/models/buildergrid.py
@@ -4,7 +4,8 @@ buildergrids = db.Table(
   "buildergrids",
   db.Column("builder_id", db.Integer, db.ForeignKey(add_prefix_for_prod("builders.id")), primary_key = True),
   db.Column("champion_id", db.Integer, db.ForeignKey(add_prefix_for_prod("champions.id")), primary_key= True),
-  db.Column("grid_index", db.Integer))
-)
+  db.Column("grid_index", db.Integer)
+  )
+
 if environment == "production":
   buildergrids.schema = SCHEMA
